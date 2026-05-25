7c47c124b03a8ee8b4dd18102e5934879d54620b
igsamchenko-cmyk <igsamchenko-cmyk@users.noreply.github.com>
2026-05-25 07:19:04 +0300

    Add everyday Ukrainian products

--- src/data/products/index.js
+++ src/data/products/index.js
@@ -1,9 +1,11 @@
 import { ukrainianProductSeeds } from '../ukrainianProductSeeds.js';
+import { everydayUkrainianProducts } from './everydayUkrainianProducts.js';
 import { expandedUkrainianProducts } from './expandedUkrainianProducts.js';
 import { importedProducts } from './importedProducts.js';
 
 export const productCatalog = [
   ...ukrainianProductSeeds,
   ...expandedUkrainianProducts,
+  ...everydayUkrainianProducts,
   ...importedProducts
 ];
