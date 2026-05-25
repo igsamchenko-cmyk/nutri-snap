f2d6c822bb55f2c7b00806c8bf21cd814f044136
igsamchenko-cmyk <igsamchenko-cmyk@users.noreply.github.com>
2026-05-25 19:38:56 +0300

    Add Ukrainian retail product pack

--- src/data/products/index.js
+++ src/data/products/index.js
@@ -3,11 +3,13 @@ import { ukrainianProductSeeds } from '../ukrainianProductSeeds.js';
 import { everydayUkrainianProducts } from './everydayUkrainianProducts.js';
 import { expandedUkrainianProducts } from './expandedUkrainianProducts.js';
 import { importedProducts } from './importedProducts.js';
+import { retailUkrainianProducts } from './retailUkrainianProducts.js';
 
 export const productCatalog = [
   ...ukrainianProductSeeds,
   ...atbProducts,
   ...expandedUkrainianProducts,
   ...everydayUkrainianProducts,
+  ...retailUkrainianProducts,
   ...importedProducts
 ];
