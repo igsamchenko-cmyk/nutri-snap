f711603ad667faa82c0cc31ee61a34535f8140dc
igsamchenko-cmyk <igsamchenko-cmyk@users.noreply.github.com>
2026-05-25 07:25:47 +0300

    Add ATB product catalog pack

--- src/data/products/index.js
+++ src/data/products/index.js
@@ -1,3 +1,4 @@
+import { atbProducts } from './atbProducts.js';
 import { ukrainianProductSeeds } from '../ukrainianProductSeeds.js';
 import { everydayUkrainianProducts } from './everydayUkrainianProducts.js';
 import { expandedUkrainianProducts } from './expandedUkrainianProducts.js';
@@ -5,6 +6,7 @@ import { importedProducts } from './importedProducts.js';
 
 export const productCatalog = [
   ...ukrainianProductSeeds,
+  ...atbProducts,
   ...expandedUkrainianProducts,
   ...everydayUkrainianProducts,
   ...importedProducts
